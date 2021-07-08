42188a6d34c86c8faa254b59eb09b6940c2887c9
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2021-07-08 15:21:37 +0800

    Add files via upload
    upload src

---
+++ vue-material-dashboard-master/src/components/Tables/OrderedTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="編號">{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="發布單位">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="公告標題">{{ item.title }}</md-table-cell>
+        <md-table-cell md-label="發布日期">{{ item.date }}</md-table-cell>
+        <!-- <md-table-cell md-label="發布日期">{{ item.city }}</md-table-cell> -->
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "ordered-table",
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      selected: [],
+      users: [],
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:3000/OrderedTable_data').then((res)=>{
+      //console.log(res.data);
+      this.users=res.data;
+    })
+  },
+};
+</script>
---
+++ vue-material-dashboard-master/src/pages/UserProfile/UserCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-card class="md-card-profile">
+    <div class="md-card-avatar">
+      <img class="img" :src="cardUserImage" />
+    </div>
+
+    <md-card-content>
+      <h6 class="category text-gray">學生</h6>
+      <h4 class="card-title">簡蔚驊</h4>
+      <p class="card-description">
+        我是電神，全資工系我最電。
+      </p>
+      <md-button class="md-round md-success">Follow</md-button>
+    </md-card-content>
+  </md-card>
+</template>
+<script>
+export default {
+  name: "user-card",
+  props: {
+    cardUserImage: {
+      type: String,
+      default: require("@/assets/img/faces/marc.jpg")
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style></style>

d8957f5128ffeb4fafa1ee6d72eb870cbc1c7b47
jimmyhealer <yahing6066@gmail.com>
2021-07-09 13:10:17 +0800

    add core for User Manager

---
+++ votebd/core/models.py
@@ -0,0 +1,42 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import (
+  AbstractBaseUser, BaseUserManager
+)
+
+class UserManager(BaseUserManager):
+  
+  def create_user(self, email, name, password=None):
+    """
+    Create and save a user with the given email, name and password.
+    """
+    if not email:
+      raise ValueError('Users must have an email address')
+
+    user = self.model(
+      email = self.normalize_email(email),
+      name = name
+    )
+
+    user.set_password(password)
+    user.save()
+    return user
+
+class User(AbstractBaseUser):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  email = models.EmailField(
+    verbose_name = 'email address',
+    max_length = 255,
+    unique = True,
+  )
+  name = models.CharField(max_length=32, blank=False, null=False)
+  is_active = models.BooleanField(default=True)
+
+  objects = UserManager()
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name']
+
+  def __str__(self):
+    return self.email

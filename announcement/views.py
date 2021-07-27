f1a2b1a66c9342ff7344587b3545b02e227ee059
jimmyhealer <yahing6066@gmail.com>
2021-07-27 21:04:08 +0800

    update apiview

--- announcement/views.py
+++ announcement/views.py
@@ -1,22 +1,8 @@
-# from rest_framework import status
-# from rest_framework.views import APIView
-from rest_framework import serializers, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from django.http import Http404, HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic.edit import CreateView
-
 from announcement.models import Announcement
 from announcement.serializers import AnnouncementSerializer
 
 from votebd.core.decorators import login_required
-
 from utils.api import APIView
-from votebd.core.models import User
-from votebd.core.serializers import UserListSerializers
-
 
 class AnnouncementList(APIView):
     """
@@ -26,15 +12,15 @@ class AnnouncementList(APIView):
     def get(self, request, format = None):
         announcement = Announcement.objects.all()
         serializer = AnnouncementSerializer(announcement, many = True)
-        return self.success(serializer.data)
+        return self.success(data = serializer.data)
 
     @login_required
     def post(self, request):
         serializer = AnnouncementSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return self.success(data = serializer.data, status = 201)
+        return self.error(msg = serializer.errors, status = 400)
 
 
 class AnnouncementDetail(APIView):
@@ -45,12 +31,12 @@ class AnnouncementDetail(APIView):
         try:
             return Announcement.objects.get(pk = pk)
         except Announcement.DoesNotExist:
-            return Http404
+            return self.error(status = 404)
 
     def get(self, request, pk, format = None):
         announcement = self.get_object(pk)
         serializer = AnnouncementSerializer(announcement)
-        return Response(serializer.data)
+        return self.success(data = serializer.data)
 
     @login_required
     def put(self, request, pk, format = None):
@@ -58,11 +44,11 @@ class AnnouncementDetail(APIView):
         serializer = AnnouncementSerializer(announcement, data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return self.success(data = serializer.data)
+        return self.error(msg = serializer.errors, status = 400)
 
     @login_required
     def delete(self, request, pk, format = None):
         announcement = self.get_object(pk)
         announcement.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return self.success(status = 204)

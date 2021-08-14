9345b804ac61fbdbbdfa9d2b55397da8c9184ecc
Timyu0527 <st970387@gmail.com>
2021-08-14 18:12:08 +0800

    pagination0

--- voteEvent/serializers.py
+++ voteEvent/serializers.py
@@ -1,7 +1,4 @@
-from django.db.models.query import QuerySet
-from announcement.models import Announcement
 from voteEvent.models import Choice, VoteEvent
-from announcement.serializers import AnnouncementSerializer
 from rest_framework import serializers
 
 class ChoiceSerializer(serializers.ModelSerializer):
@@ -9,12 +6,11 @@ class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ['id', 'choice_text', 'votes']
-        depth = 2
+        depth = 1
 
 class VoteEventSerializer(serializers.ModelSerializer):
     
     choices = ChoiceSerializer(source = 'choice_set', many = True)
-    #announcement = AnnouncementSerializer(source = 'announcement_set')
 
     class Meta:
         model = VoteEvent
@@ -22,10 +18,7 @@ class VoteEventSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         choices_data = validated_data.pop('choice_set')
-        #announcement_data = validated_data.pop('announcement_set')
         voteEvent = VoteEvent.objects.create(**validated_data)
-        #for _announcement_data in announcement_data:
-        #Announcement.objects.create(voteEvent = voteEvent, **announcement_data)
         for choice_data in choices_data:
             Choice.objects.create(voteEvent = voteEvent, **choice_data)
         return voteEvent
@@ -34,7 +27,6 @@ class VoteEventSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
         instance.content = validated_data.get('content', instance.content)
-        #instance.announcement = validated_data.get('announcement', instance.announcement)
         instance.save()
 
         choices = validated_data.get('choice_set')
--- voteEvent/views.py
+++ voteEvent/views.py
@@ -3,6 +3,7 @@ from django.http.response import Http404, HttpResponse
 
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from votebd.core.decorators import login_required
@@ -10,15 +11,27 @@ from announcement.serializers import AnnouncementSerializer
 from announcement.models import Announcement
 from voteEvent.serializers import VoteEventSerializer
 from voteEvent.models import VoteEvent
+from voteEvent.pagination import PaginationHandlerMixin
 
 # Create your views here.
 
-class VoteEventList(APIView):
+class BasicPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'limit'
+    max_page_size = 10
 
+class VoteEventList(APIView, PaginationHandlerMixin):
+    pagination_class = BasicPagination
+    
     def get(self, request):
         all_voteEvent = VoteEvent.objects.all()
+        page = self.paginate_queryset(all_voteEvent)
+        if page is not None:
+            serializer = self.get_paginated_response(VoteEventSerializer(page, many = True).data)
+        else:
+            serializer = VoteEventSerializer(all_voteEvent, many = True)
+
         #print(all_voteEvent)
-        serializer = VoteEventSerializer(all_voteEvent, many = True)
         return Response(serializer.data)
 
     @login_required
